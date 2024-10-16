30790600c53001dcb41865fd7b9c4433e580ef69
Sangmin Lee <sart5534@gmail.com>
2024-10-16 10:36:10 +0900

    [refactor]: 인증 클래스 주입 RoleValidator 클래스를 분리

--- src/main/java/com/sparta/sunday/domain/list/service/ListService.java
+++ src/main/java/com/sparta/sunday/domain/list/service/ListService.java
@@ -1,15 +1,14 @@
 package com.sparta.sunday.domain.list.service;
 
+import com.sparta.sunday.config.RoleValidator;
 import com.sparta.sunday.domain.board.entity.Board;
 import com.sparta.sunday.domain.board.repository.BoardRepository;
 import com.sparta.sunday.domain.common.dto.AuthUser;
-import com.sparta.sunday.domain.common.exception.ReadOnlyRoleException;
 import com.sparta.sunday.domain.list.dto.request.ListRequest;
 import com.sparta.sunday.domain.list.dto.response.ListResponse;
 import com.sparta.sunday.domain.list.entity.BoardList;
 import com.sparta.sunday.domain.list.repository.ListRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +19,12 @@ public class ListService {
 
     private final ListRepository listRepository;
     private final BoardRepository boardRepository;
+    private final RoleValidator roleValidator;
 
     @Transactional
-    public ListResponse saveList(Long boardId, AuthUser authUser, ListRequest listRequest){
+    public ListResponse saveList(Long boardId, AuthUser authUser, ListRequest listRequest) {
 
-        validateRole(authUser);
+        roleValidator.validateRole(authUser);
 
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 보드가 없습니다"));
@@ -45,7 +45,7 @@ public class ListService {
     @Transactional
     public ListResponse updateList(Long listId, AuthUser authUser, ListRequest listRequest) {
 
-        validateRole(authUser);
+        roleValidator.validateRole(authUser);
         BoardList boardList = listRepository.findById(listId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 리스트가 없습니다"));
 
@@ -56,18 +56,11 @@ public class ListService {
     @Transactional
     public void deleteList(Long listId, AuthUser authUser) {
 
-        validateRole(authUser);
+        roleValidator.validateRole(authUser);
         BoardList boardList = listRepository.findById(listId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 리스트가 없습니다"));
 
         listRepository.delete(boardList);
     }
-
-    private void validateRole(AuthUser authUser) {
-        if (authUser.getAuthorities().contains(new SimpleGrantedAuthority("MEMBER"))) {
-            throw new ReadOnlyRoleException("읽기 전용 멤버는 작업을 수행할 수 없습니다.");
-        }
-    }
-
 
 }
